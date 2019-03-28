b153364835183903f974fc3d64f93881e696263f
Andis Papageo <42977189+andispapageo@users.noreply.github.com>
2019-03-29 00:44:25 +0200

    Update DialogSetup.cs

--- BTEX2/Pages/DialogSetup.cs
+++ BTEX2/Pages/DialogSetup.cs
@@ -1,4 +1,3 @@
-﻿
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -11,7 +10,6 @@ using Nmnemonic = NBitcoin.Mnemonic;
 using NWorldList = NBitcoin.Wordlist;
 using NBitcoin;
 using static BitCWallet.MnemonicEx1;
-
 namespace BitCWallet.Pages
 {
     public class DialogSetup : PMADialog
@@ -22,7 +20,6 @@ namespace BitCWallet.Pages
         TextInputLayout password { get { return mainMnemonice.FindViewById<TextInputLayout>(Resource.Id.txtInputLayoutPassword); } }
         Button login { get { return mainMnemonice.FindViewById<Button>(Resource.Id.btnLogin); } }
         Button restore { get { return mainMnemonice.FindViewById<Button>(Resource.Id.btnRestore); } }
-
         public string walletWords { get; private set; }
         public int keyA { get; private set; }
 
@@ -66,16 +63,8 @@ namespace BitCWallet.Pages
         {
             BitcoinPassphraseCode passphraseCode = new BitcoinPassphraseCode(password,MainActivity.net, null);
             EncryptedKeyResult encryptedKey1 = passphraseCode.GenerateEncryptedSecret();
-            
-            Console.WriteLine(encryptedKey1.GeneratedAddress);
-            Console.WriteLine(encryptedKey1.EncryptedKey);
-            Console.WriteLine(encryptedKey1.ConfirmationCode);
-
-            Console.WriteLine(encryptedKey1.ConfirmationCode.Check(password, encryptedKey1.GeneratedAddress));
             BitcoinSecret privateKey = encryptedKey1.EncryptedKey.GetSecret(password);
-            Console.WriteLine(privateKey.GetAddress() == encryptedKey1.GeneratedAddress);
             return encryptedKey1.EncryptedKey.ToString();
-
         }
         private async Task<string> CreateWallet()
         {
@@ -87,11 +76,7 @@ namespace BitCWallet.Pages
                     var mnemonic = new Nmnemonic(NWorldList.English, WordCount.Twelve);
                     ssMnemo = mnemonic.ToString();
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    ssMnemo = string.Empty;
-                }
+                catch (Exception e){Console.WriteLine(e.Message); ssMnemo = string.Empty;}
             });
             return await Task.FromResult(ssMnemo);
         }
@@ -100,24 +85,16 @@ namespace BitCWallet.Pages
         {
             Wordlist mWordlist = new Wordlist();
             var mnemonic = new MnemonicEx1(Wordlist.English, NWordCount.Twelve);
-            if (mnemonic == null)
-                return;
-
+            if (mnemonic == null)return;
             var res = mWordlist.GetSentence(mnemonic._Indices);
-            if (string.IsNullOrWhiteSpace(res))
-                return;
-
+            if (string.IsNullOrWhiteSpace(res))return;
             var clearres = res.Replace("\0", " ").Split(' ');
             var resPrivatekey = mnemonic.PrivateKeyAddress;
             var showDialog = new DialogShowWords();
             res = res.Replace("\0", " ");
             showDialog.mSeed = res;
             showDialog.Show(MainActivity.fm, "seed");
-
-            MainActivity.Preferences.Edit().PutString("mnemonic", res).Apply();
-            MainActivity.Preferences.Edit().PutString("PrKey", resPrivatekey).Apply();
         }
-
 
         public IEnumerable<T> GetViewsByType<T>(ViewGroup root) where T : View
         {
