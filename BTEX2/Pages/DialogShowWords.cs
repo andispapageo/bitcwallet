8bd1dea1e899b61173566c10c71a3b9028c24b76
Andis Papageo <42977189+andispapageo@users.noreply.github.com>
2019-03-29 00:43:19 +0200

    Update DialogShowWords.cs

--- BTEX2/Pages/DialogShowWords.cs
+++ BTEX2/Pages/DialogShowWords.cs
@@ -9,7 +9,7 @@ namespace BitCWallet.Pages
     public class DialogShowWords : PMADialog
     {
         View view;
-        TextView words { get { return view.FindViewById(Resource.Id.words) as TextView; } }
+        TextView words { get { return view.FindViewById(Resource.Id.words) as TextView; }}
         public string mSeed { get; set; }
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -17,13 +17,11 @@ namespace BitCWallet.Pages
             words.Text = mSeed;
             return view;
         }
-
         public override void OnStop()
         {
             base.OnStop();
             if (InitialisePages.mWalletsPage == null) return;
             InitialisePages.mWalletsPage.AdjustBitCoinAddress();
         }
-
     }
 }
